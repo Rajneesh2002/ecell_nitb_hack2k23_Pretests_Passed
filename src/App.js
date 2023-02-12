c96590692a282bed1667f6f4c72fcb26802e292d
Rajneesh Kushwaha <92571822+Rajneesh2002@users.noreply.github.com>
2023-02-12 09:56:23 +0530

    Commit-6_sidebar_done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import React from "react"
 import {  Route, Routes } from "react-router-dom";
 
@@ -12,10 +13,41 @@ function App() {
   return (
     <>
     <Layout/>
-    
-    
+   
     </>
   );
 }
+
+export default App;
+*/
+
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar1'
+import Dashboard from './pages/Dashboard.jsx';
+import About from './pages/About.jsx';
+import Analytics from './pages/Analytics.jsx';
+import Comment from './pages/Comment.jsx';
+import Product from './pages/Product.jsx';
+import ProductList from './pages/ProductList.jsx';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
+  );
+};
 
 export default App;
